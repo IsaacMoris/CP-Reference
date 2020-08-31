caf4dc8b2afa4468f50ccbc568b6945675bcbebc
IsaacMoris <48593866+IsaacMoris@users.noreply.github.com>
2020-08-31 06:03:13 +0200

    Update LiChaoTree With Pointers.cpp

--- LiChaoTree With Pointers.cpp
+++ LiChaoTree With Pointers.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include <bits/stdc++.h>
 #define ll long long
-#define ld  double
+#define ld  long double
 #define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 struct Line
