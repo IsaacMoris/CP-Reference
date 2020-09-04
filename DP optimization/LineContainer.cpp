22ea1edaf3120fa591b13592be86b2c3a7675f74
IsaacMoris <48593866+IsaacMoris@users.noreply.github.com>
2020-09-05 01:46:25 +0200

    Update LineContainer.cpp

--- DP optimization/LineContainer.cpp
+++ DP optimization/LineContainer.cpp
@@ -1,42 +1,51 @@
-/**
- * Author: Simon Lindholm
- * Date: 2017-04-20
- * License: CC0
- * Source: own work
- * Description: Container where you can add lines of the form kx+m, and query maximum values at points x.
- *  Useful for dynamic programming (``convex hull trick'').
- * Time: O(\log N)
- * Status: stress-tested
- */
-#pragma once
-
-struct Line {
-	mutable ll k, m, p;
-	bool operator<(const Line& o) const { return k < o.k; }
-	bool operator<(ll x) const { return p < x; }
+struct Line
+{
+    mutable ll m, b, p;
+    bool operator<(const Line& o) const
+    {
+        return m < o.m;
+    }
+    bool operator<(ll x) const
+    {
+        return p < x;
+    }
 };
 
-struct LineContainer : multiset<Line, less<>> {
-	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
-	static const ll inf = LLONG_MAX;
-	ll div(ll a, ll b) { // floored division
-		return a / b - ((a ^ b) < 0 && a % b); }
-	bool isect(iterator x, iterator y) {
-		if (y == end()) { x->p = inf; return false; }
-		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
-		else x->p = div(y->m - x->m, x->k - y->k);
-		return x->p >= y->p;
-	}
-	void add(ll k, ll m) {
-		auto z = insert({k, m, 0}), y = z++, x = y;
-		while (isect(y, z)) z = erase(z);
-		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
-		while ((y = x) != begin() && (--x)->p >= y->p)
-			isect(x, erase(y));
-	}
-	ll query(ll x) {
-		assert(!empty());
-		auto l = *lower_bound(x);
-		return l.k * x + l.m;
-	}
+struct LineContainer : multiset<Line, less<>>
+{
+    // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    static const ll inf = LLONG_MAX;
+    ll div(ll db, ll dm)   // floored division
+    {
+        return db / dm - ((db ^ dm) < 0 && db % dm);
+    }
+    bool isect(iterator x, iterator y)
+    {
+        if (y == end())
+        {
+            x->p = inf;
+            return false;
+        }
+        if (x->m == y->m)
+            x->p = x->b > y->b ? inf : -inf;
+        else
+            x->p = div(y->b - x->b, x->m - y->m);
+        return x->p >= y->p;
+    }
+    void add(ll m, ll b)
+    {
+        auto z = insert({m, b, 0}), y = z++, x = y;
+        while (isect(y, z))
+            z = erase(z);
+        if (x != begin() && isect(--x, y))
+            isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+    ll query(ll x)
+    {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.m * x + l.b;
+    }
 };
