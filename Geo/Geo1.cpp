6afd93d94f833d3ecb8d36f4edc2418cf1ad3877
IsaacMoris <48593866+IsaacMoris@users.noreply.github.com>
2020-11-12 01:10:54 +0200

    Update Geo1.cpp

--- Geo/Geo1.cpp
+++ Geo/Geo1.cpp
@@ -54,7 +54,7 @@ point reflectAroundLine(point a, point b, point p) {
     return ProjectPointLine(a,b,p)*2 - p;
 }
 
-point RotateCCW(point p, long double t) {
+point RotateCCW(point p, long double t /* (radian) */) {
     return point(p.x * cos(t) - p.y * sin(t),
                  p.x * sin(t) + p.y * cos(t));
 }
