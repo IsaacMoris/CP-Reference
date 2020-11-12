415ea96d7782f1733b03d5576d53d9dfe3af60e5
IsaacMoris <48593866+IsaacMoris@users.noreply.github.com>
2020-11-12 19:18:45 +0200

    Update Geo1.cpp

--- Geo/Geo1.cpp
+++ Geo/Geo1.cpp
@@ -4,18 +4,18 @@
 #define ld  long double
 #define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-using  ptype =  long long edit this first ;
+using  ptype =  long long ;
 struct point {
 
     ptype x,  y;
     point(ptype x, ptype y) : x(x), y(y) {}
 
     point operator -(const point & other)const {
-        return point(x-other.x, y-other.y);
+        return point(x - other.x, y - other.y);
     }
 
     point operator +(const point & other)const {
-        return point(x+other.x, y+other.y);
+        return point(x + other.x, y + other.y);
     }
 
     point operator *(ptype c) const {
@@ -26,35 +26,45 @@ struct point {
         return point(x / c, y / c);
     }
 
-
 };
 ptype cross(point a, point b) {
-    return a.x*b.y-a.y*b.x;
+    return a.x * b.y - a.y * b.x;
 }
 
 ptype dot(point a, point b) {
-    return a.x*b.x+a.y*b.y;
+    return a.x * b.x + a.y * b.y;
 }
-
+double abs(point a) {
+    return sqrt(dot(a, a));
+}
+// angle between [0 , pi]
+double angle (point a, point b) {
+    return acos(dot(a, b) / abs(a) / abs(b));
+}
+// a : point in Line
+// d : Line slope
+point LineLineIntersect(point a1, point d1, point a2, point d2) {
+    return a1 + d1 * cross(a2 - a1, d2) / cross(d1, d2);
+}
 point ProjectPointLine(point a, point b, point c) {
-    return a + (b-a) * dot(c-a,b-a)/dot(b-a,b-a);
+    return a + (b - a) * 1.0 * dot(c - a, b - a) / dot(b - a, b - a);
 }
 
 point ProjectPointSegment(point a, point b, point c) {
-    ptype r=dot(c-a,b-a)/dot(b-a,b-a);
-    if(r<0)
+    double r = dot(c - a, b - a) / dot(b - a, b - a);
+    if(r < 0)
         return a;
-    if(r>1)
+    if(r > 1)
         return b;
-    return a+ (b-a) * r ;
+    return a + (b - a) * r ;
 }
 
 point reflectAroundLine(point a, point b, point p) {
     //(proj-p) *2 + p
-    return ProjectPointLine(a,b,p)*2 - p;
+    return ProjectPointLine(a, b, p) * 2 - p;
 }
 
-point RotateCCW(point p, long double t /* (radian) */) {
+point RotateCCW(point p, double t) {
     return point(p.x * cos(t) - p.y * sin(t),
                  p.x * sin(t) + p.y * cos(t));
 }
