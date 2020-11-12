2b798ee2571251a9556ef80e05da54de0abea856
IsaacMoris <48593866+IsaacMoris@users.noreply.github.com>
2020-11-13 01:54:48 +0200

    Create Euler's totient function.cpp

---
+++ Algebra/Euler's totient function.cpp
@@ -0,0 +1,14 @@
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
